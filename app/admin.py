0661a48b8333423e708bca5ee030b26a44dfb240
CarolM0rais <caroldemorais07@gmail.com>
2025-05-05 14:45:09 -0300

    PSOO2

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import *
-from django.contrib import admin
+from .models import Cidade, Autor, Editora, Leitor, Livro, Genero
 
+# Inline de Livro no admin do Autor
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1  # Número de livros adicionais visíveis por padrão
+
+# Configuração personalizada para o admin de Autor
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    inlines = [LivroInline]  # Exibe livros diretamente no admin de Autor
+
+# Registro dos modelos no admin
 admin.site.register(Cidade)
-admin.site.register(Autor)
+admin.site.register(Autor, AutorAdmin)  # Usa a versão personalizada com inline
 admin.site.register(Editora)
 admin.site.register(Leitor)
 admin.site.register(Livro)
